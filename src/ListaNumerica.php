a985406e74ede2ee11af4e0e818f2d9c78518280
Gustavo Oliveira da Motta <gumotta2908@hotmail.com>
2024-02-16 16:15:57 -0300

    Atualizado 16:15

--- src/ListaNumerica.php
+++ src/ListaNumerica.php
@@ -5,7 +5,6 @@ class ListaNumerica
 
     public function somarElementos(array $lista)
     {
-
         $quantidade_elementos = count($lista);
 
         if ($quantidade_elementos == 0) {
@@ -35,6 +34,7 @@ class ListaNumerica
     public function encontrarMenorElemento(array $lista)
     {
         $quantidade_elementos = count($lista);
+
         if ($quantidade_elementos == 0) {
             return false;
 
@@ -67,11 +67,15 @@ class ListaNumerica
         }
         
         $array_pares = [];
+
         for ($posicao = 0; $posicao < $quantidade_elementos; $posicao++) {
-            if ($lista[$posicao] % 2 == 0) {
+            if ($lista[$posicao] %2 == 0) {
                 $array_pares [] = $lista[$posicao];
             }
         }
         return $array_pares;
     }
 }
+//Nessa classe estão todas as funções pedidas no PDF, optei por utilizar as funções do próprio PHP para economizar espaço e tempo / nao sei se é a opção mais performática, creio que não
+// Todas as funções validam se o array tem 0 elementos ou se tem apenas 1 elemento, com exceção da function filtrarNumerosPares que apenas valida se o array está vazio
+// Não é necessário adicionar uma validação para vários elementos pois caso houver 1 elemento ou 0 elementos o programa ja retorna
--- tests/ListaNumericaTest.php
+++ tests/ListaNumericaTest.php
@@ -9,72 +9,96 @@ require_once "src/ListaNumerica.php";
 
 class ListaNumericaTest extends TestCase
 {
-    public $lista_numerica;
+    public $listaNumerica;
     public function setUp(): void
     {
-        $this->lista_numerica = new ListaNumerica;
+        $this->listaNumerica = new ListaNumerica;
     }
 
     public function testSomarElementos()
     {
-        $array = [1, 3, 5, 7];
-        assertEquals(16, $this->lista_numerica->somarElementos($array));
+        $array = [1, 3, 5, 7 , 0];
+        assertEquals(16, $this->listaNumerica->somarElementos($array));
+
+        $array = [1, -3, -5, 7, -10, 0];
+        assertEquals(-10, $this->listaNumerica->somarElementos($array));
 
         $array = [];
-        assertFalse($this->lista_numerica->somarElementos($array));
+        assertFalse($this->listaNumerica->somarElementos($array));
 
         $array = [1];
-        assertEquals(1, $this->lista_numerica->somarElementos($array));
+        assertEquals(1, $this->listaNumerica->somarElementos($array));
     }
     public function testEncontraMaiorElemento()
     {
         $array = [1, 3, 5, 7];
-        assertEquals(7, $this->lista_numerica->encontrarMaiorElemento($array));
+        assertEquals(7, $this->listaNumerica->encontrarMaiorElemento($array));
+
+        $array = [-1, -3, -5, -7];
+        assertEquals(-1, $this->listaNumerica->encontrarMaiorElemento($array));
 
         $array = [];
-        assertFalse($this->lista_numerica->encontrarMaiorElemento($array));
+        assertFalse($this->listaNumerica->encontrarMaiorElemento($array));
 
         $array = [5];
-        assertEquals(5, $this->lista_numerica->encontrarMaiorElemento($array));
+        assertEquals(5, $this->listaNumerica->encontrarMaiorElemento($array));
     }
     public function testEncontraMenorElemento()
     {
         $array = [1, 3, 5, 7];
-        assertEquals(1, $this->lista_numerica->encontrarMenorElemento($array));
+        assertEquals(1, $this->listaNumerica->encontrarMenorElemento($array));
+
+        $array = [-1,-3, -5, -7];
+        assertEquals(-7, $this->listaNumerica->encontrarMenorElemento($array));
 
         $array = [];
-        assertFalse($this->lista_numerica->encontrarMenorElemento($array));
+        assertFalse($this->listaNumerica->encontrarMenorElemento($array));
 
         $array = [5];
-        assertEquals(5, $this->lista_numerica->encontrarMenorElemento($array));
+        assertEquals(5, $this->listaNumerica->encontrarMenorElemento($array));
     }
     public function testOrdenarLista()
     {
         $array = [3, 1, 6, 5];
-        $esperado = [1, 3, 5, 6];
-        assertEquals($esperado, $this->lista_numerica->ordenarLista($array));
+        $resultadoEsperado = [1, 3, 5, 6];
+        assertEquals($resultadoEsperado, $this->listaNumerica->ordenarLista($array));
+
+        $array = [-3, -1, -6, -5];
+        $resultadoEsperado = [-6, -5, -3, -1];
+        assertEquals($resultadoEsperado, $this->listaNumerica->ordenarLista($array));
 
         $array = [];
-        assertFalse($this->lista_numerica->ordenarLista($array));
+        assertFalse($this->listaNumerica->ordenarLista($array));
 
         $array = [6];
-        assertEquals([6], $this->lista_numerica->ordenarLista($array));
+        assertEquals([6], $this->listaNumerica->ordenarLista($array));
 
     }
      public function testFiltrarNumerosPares()
     {
         $array = [3, 1, 8, 6, 5];
-        $array_esperado = [8,6];
-        assertEquals($array_esperado, $this->lista_numerica->filtrarNumerosPares($array));
+        $array_resultadoEsperado = [8,6];
+        assertEquals($array_resultadoEsperado, $this->listaNumerica->filtrarNumerosPares($array));
+
+        $array = [3, 1, -8, -6, 5];
+        $array_resultadoEsperado = [-8,-6];
+        assertEquals($array_resultadoEsperado, $this->listaNumerica->filtrarNumerosPares($array));
 
         $array = [];
-        assertFalse($this->lista_numerica->filtrarNumerosPares($array));
+        assertFalse($this->listaNumerica->filtrarNumerosPares($array));
 
         $array = [6];
-        assertEquals([6], $this->lista_numerica->ordenarLista($array));
+        assertEquals([6], $this->listaNumerica->filtrarNumerosPares($array));
 
-        $array = [5];
-        assertEquals([], $this->lista_numerica->ordenarLista($array));
+        $array = [11];
+        assertEquals([], $this->listaNumerica->filtrarNumerosPares($array));
 
     }
 }
+//Foi usado a function setUp para declarar que a variável listaNumerica é um objeto da classe ListaNumerica
+// Cada function de teste contém 4 testes, com exceção da filtrarNumerosPares que tem 5 testes
+//Todas as functions validam um array com: Elementos positivos; Elementos negativos; Nenhum elemento; 1 elemento apenas; Todas estão retornando os resultados corretos
+// A testFiltrarNumerosPares tem 1 teste a mais pois eu testei um array com 1 elemento ímpar, para ver se ela retornava corretamente, e está tudo certo.
+//Sei que eu poderia ter feito de maneira mais concisa, por exemplo, eu fiz validações para positivos e negativos, 
+//porém se eu misturasse negativos com positivos nas functions de soma, maior, menor e listagem o programa iria validar corretamente também e com menos linhas
+//optei por deixar separado para facilitar a leitura e ver que as operações estão funcionando corretamente
